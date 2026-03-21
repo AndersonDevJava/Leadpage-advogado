85de720c277e46946a87e93228293912739ba13d
AwebSystemGM <awebsystemagj@gmail.com>
2026-03-20 21:53:24 -0300

    fix: ajusta deploy no render, remove frontend do backend, configura prisma e endpoint de produção

--- src/public/conectionAPI.js
+++ src/public/conectionAPI.js
@@ -25,7 +25,7 @@ form.addEventListener("submit", async (e) => {
     }
 
     try {
-        const response = await fetch("http://localhost:3333/users", {
+        const response = await fetch("https://leadpage-advogado-1.onrender.com", {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
