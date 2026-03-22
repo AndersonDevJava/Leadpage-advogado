1541eb471c9c28f247da02b9dfafddd40907f86a
AwebSystemGM <awebsystemagj@gmail.com>
2026-03-22 18:32:15 -0300

    feat: add test-db route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { router } from './src/api/routes/router.js'
 import cors from 'cors'
+import { prisma } from './src/lib/conectionDataBase.js'
 
 const app = express()
 
@@ -8,6 +9,22 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/users', router)
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const contatos = await prisma.contato.findMany()
+    res.json({
+      status: 'OK',
+      total: contatos.length,
+      dados: contatos
+    })
+  } catch (error) {
+    res.status(500).json({
+      erro: error.message,
+      code: error.code
+    })
+  }
+})
 
 const PORT = process.env.PORT || 3333;
 
